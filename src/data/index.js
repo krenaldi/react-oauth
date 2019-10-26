44974c850e89f8f5f89930ea7c54f8644fe9dbdb
Kris Renaldi <krenaldi@comcast.net>
2019-10-26 11:10:22 -0400

    Removed unnecesary login options

--- src/data/index.js
+++ src/data/index.js
@@ -1,19 +1,9 @@
-import AmazonIcon from "../res/amazon.png";
 import FacebookIcon from "../res/facebook.png";
 import GithubIcon from "../res/github.png";
 import GoogleIcon from "../res/google.png";
-import InstagramIcon from "../res/instagram.png";
-import SpotifyIcon from "../res/spotify.png";
-import TwitchIcon from "../res/twitch.png";
+
 
 export const data = [{
-    img: AmazonIcon,
-    name: "amazon",
-    href: "auth/amazon",
-    alt: "amazon-icon",
-    color: "#F9AE31",
-    txt: "Login with Amazon"
-},  {
     img: FacebookIcon,
     name: "facebook",
     href: "auth/facebook",
@@ -34,32 +24,4 @@ export const data = [{
     alt: "google-icon",
     color: "#CB4024",
     txt: "Login with Google"
-}, {
-    img: InstagramIcon,
-    name: "instagram",
-    href: "auth/instagram",
-    alt: "instagram-icon",
-    colors: {
-        leftBot: "#fec564",
-        leftTop: "#5258cf",
-        rightTop: "#893dc2",
-        rightBot: "#d9317a",
-        baseCoat: "linear-gradient(#6559ca, #bc318f 30%, #e33f5f 50%, #f77638 70%, #fec66d 100%)",
-    },
-    color: "#d9317a",
-    txt: "Login with Instagram"
-}, {
-    img: SpotifyIcon,
-    name: "spotify",
-    href: "auth/spotify",
-    alt: "spotify-icon",
-    color: "#1EB954",
-    txt: "Login with Spotify"
-}, {
-    img: TwitchIcon,
-    name: "twitch.js",
-    href: "auth/twitch",
-    alt: "twitch-icon",
-    color: "#5F3BAD",
-    txt: "Login with Twitch"
 }];
