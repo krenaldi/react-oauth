697542c2ffa7c64dac4570d16588c81b66428e45
Andreas Nicolaou <49968813+agnicolaou@users.noreply.github.com>
2019-10-31 21:02:02 -0400

    Updated home page. Additional styling in sass. Updated cards.js.

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,22 +4,29 @@ import CardList from '../components/cards/CardList';
 
 const Home = () => {
     return (
-        <div className="page" style={{ textAlign: "center"}}>
-            <p className="page-title">Simple OAuth with Node</p>
-            <p style={{ fontSize: 20 }}>
-                Login with Facebook, Google, or Github.
+        <div className="page" style={{ textAlign: "center" }}>
+            <h1 className="page-title">Welcome to Common Grounds</h1>
+            <h5 className="subtitle" style={{ fontSize: 20 }}>
+                where everybody meets half way
+            </h5>
+
+            <p>
+                Introducing an easy and fun way to schedule a public get-together without the usual hassle. Whether you're meetig up with colleagues or hanging out with friends, Common Grounds provides your group with destinations in the center of each member's location.
             </p>
-            <Terminal 
-                userData={"passport.authenticate('facebook')"} 
-                selected="All"
-            />
+
+            <p>
+                Explore new areas, discover your new favorite restaurant, and share great times with your friends on Common Grounds.
+            </p>
+            
             <p style={{ fontSize: 28 }}>
                 Popular Login Strategies
             </p>
             <CardList />
             <div style={{ marginBottom: 20 }} />
-        </div>
+        </div >
     );
 };
 
 export default Home;
+
+
